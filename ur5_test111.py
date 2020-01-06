c72c60458631ecab3a0f9d94067444114b390bc1
NimbleKun <35164161+NimbleKun@users.noreply.github.com>
2020-01-06 14:11:11 +0800

    Add files via upload

--- ur5_test111.py
+++ ur5_test111.py
@@ -9,6 +9,10 @@ from math import *
 from random import uniform
 from trajectory_msgs.msg import JointTrajectoryPoint
 import rospy
+import tf
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+
 # [0.0, -pi/2, pi/2, pi/3, 0, -pi/10]
 # waypoints = [[uniform(-pi, pi) for _ in range(0,6)], [0,0,0,0,0,0]]
 
@@ -17,7 +21,7 @@ def main():
     rospy.init_node('send_joints')
     pub = rospy.Publisher('/arm_controller/command',
                           JointTrajectory,
-                          queue_size=10)
+                          queue_size=100)
 
 
     # Create the topic message
@@ -28,23 +32,37 @@ def main():
                         'elbow_joint', 'wrist_1_joint', 'wrist_2_joint',
                         'wrist_3_joint']
 
-    rate = rospy.Rate(1)
-    cnt = 0
+    rate = rospy.Rate(10)
     pts = JointTrajectoryPoint()
     traj.header.stamp = rospy.Time.now()
-
+    
     while not rospy.is_shutdown():
+	joint_angle_mat = load_file("/home/nimblekun/kun_ur5test/src/ur5_test/scripts/kun_5.txt")
+	cnt = 0
+	for joint_angle in joint_angle_mat:
+            pts.positions = joint_angle
+	    pts.time_from_start = rospy.Duration(0.1)
+	    # Set the points to the trajectory
+	    traj.points = []
+	    traj.points.append(pts)
+	    # Publish the message
+	    pub.publish(traj)
+	    cnt+=1
+	    print(cnt)
+	    #tra()
+	    rate.sleep()
 
-        pts.positions = [uniform(0,pi),uniform(0,-pi/2),uniform(0,pi),uniform(0,pi),uniform(0,pi),uniform(0,pi)]
-        pts.time_from_start = rospy.Duration(1.0)
-        cnt+=1
-        cnt%=2
-        # Set the points to the trajectory
-        traj.points = []
-        traj.points.append(pts)
-        # Publish the message
-        pub.publish(traj)
-        rate.sleep()
+def load_file(file_path):
+    num_mat = []
+    with open(file_path) as file:
+        for line in file.readlines():
+            line = line.rstrip()
+            num_list = line.split("\t")
+            num_list = [float(i) for i in num_list]
+            num_mat.append(num_list)
+        file.close()
+    return num_mat
+
 
 if __name__ == '__main__':
     try:
